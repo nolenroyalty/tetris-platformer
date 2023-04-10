eb92f30bb45af66f62c8b8a559fb028c626845d4
Nolen <nolen.royalty@gmail.com>
2023-04-10 00:31:24 -0400

    Better piece sliding

--- Main/TetrisComponent.gd
+++ Main/TetrisComponent.gd
@@ -16,9 +16,13 @@ const TICKDOWN_TIMER_START = 1.1
 var tetrisPiece = preload("res://Piece/Piece.tscn")
 var rowExplosion = preload("res://Effects/RowExplosion.tscn")
 onready var landscape = $Landscape
-onready var input_timer = $InputTimer
-onready var tickdown_timer = $TickdownTimer
+onready var tickdown_timer : Timer = $TickdownTimer
+onready var can_begin_sliding_timer : Timer = $CanBeginSlidingTimer
+onready var slide_again_timer : Timer = $SlideAgainTimer
+onready var level_increase_timer : Timer = $LevelIncreaseTimer
 onready var bottom_display = $BottomDisplay
+
+var dslide = Vector2.ZERO
 
 var tickdown_timer_amount = TICKDOWN_TIMER_START 
 var current_piece : Piece
@@ -74,10 +78,11 @@ func _ready():
 	spawn_current_piece()
 	tickdown_timer.set_wait_time(tickdown_timer_amount)
 
-	var levelIncreaseTimer = $LevelIncreaseTimer
-	levelIncreaseTimer.connect("timeout", self, "level_up")
-	var tickdownTimer = $TickdownTimer
-	tickdownTimer.connect("timeout", self, "handle_tickdown")
+	var _ignore
+	_ignore = level_increase_timer.connect("timeout", self, "level_up")
+	_ignore = tickdown_timer.connect("timeout", self, "handle_tickdown")
+	_ignore = can_begin_sliding_timer.connect("timeout", self, "handle_can_begin_sliding_timer")
+	_ignore = slide_again_timer.connect("timeout", self, "handle_slide_again_timer")
 
 func verify_proposed_coordinates(proposed_x, proposed_y, positions):
 	for position in calculate_absolute_positions(proposed_x, proposed_y, positions):
@@ -203,25 +208,40 @@ func reset_tickdown_timer(amount):
 		tickdown_timer.start(amount)
 		tickdown_timer.set_wait_time(tickdown_timer_amount)
 
-# There are a few things that this function should do but currently doesn't.
+func handle_slide_again_timer():
+	var proposed_x = current_x + dslide.x
+	var proposed_y = current_y + dslide.y
+	if verify_proposed_coordinates(proposed_x, proposed_y, current_piece.current_positions()):
+		set_piece_position(proposed_x, proposed_y)
+		reset_tickdown_timer(0.15)
+		redisplay_ghost()
+	# It's a little odd but I think it's correct to do nothing here - we're sliding against a wall (or whatever)
+	# and can't move, but that's fine.  The player will probably stop holding the key.
 
-# Maybe most importantly, if a piece is added to the landscape because you've pressed the "down" key
+func can_begin_sliding():
+	# When the player presses a key we kick off this timer - when it fires we enter the sliding state until
+	# the player releases the key.
+	can_begin_sliding_timer.start()
+
+func handle_can_begin_sliding_timer():
+	slide_again_timer.start()
+
+func stop_sliding():
+	can_begin_sliding_timer.stop()
+	slide_again_timer.stop()
+	dslide = Vector2(0, 0)
+
+# Something we should fix: if a piece is added to the landscape because you've pressed the "down" key
 # and we've spawned a new piece, we probably shouldn't immediately start moving that piece down the board.
 # The fact that we do this feels pretty bad when the landscape has made it to the top of the screen, as it
 # can cause you to inadvertently drop more pieces into the landscape quickly.
-
-# A harder to solve problem is that piece movement still doesn't feel quite right.  Some of this is because
-# I think we should move a piece more quickly the longer you hold down the left or right arrow keys.  But there
-# are also times where it feels like a piece continues to move after you've let go of the key.  I'm not actually
-# totally sure *why* it feels like that and fixing it would probably require some experimentation.
-
 func _process(_delta):
 	var proposed_x = current_x
 	var proposed_y = current_y
 	var attempt_a_move = false
 	var down_pressed_so_we_can_add_to_landscape = false
 	var performed_an_action = false
-	var can_move_piece = input_timer.is_stopped()
+	var can_move_piece = true
 	
 	if Input.is_action_just_pressed("drop"):
 		perform_drop()
@@ -233,23 +253,41 @@ func _process(_delta):
 		reset_tickdown_timer(0.3)
 		return	
 	
-	if Input.is_action_pressed("left") and can_move_piece:
+	if Input.is_action_just_pressed("left") and can_move_piece:
 		proposed_x -= 1
+		dslide = Vector2(-1, 0)
+		can_begin_sliding()
 		attempt_a_move = true
-	if Input.is_action_pressed("right") and can_move_piece:
+	if Input.is_action_just_pressed("right") and can_move_piece:
 		proposed_x += 1
+		dslide = Vector2(1, 0)
+		can_begin_sliding()
 		attempt_a_move = true
-	if Input.is_action_pressed("down") and can_move_piece:
+	if Input.is_action_just_pressed("down") and can_move_piece:
 		proposed_y += 1
+		dslide = Vector2(0, 1)
+		can_begin_sliding()
 		attempt_a_move = true
 		down_pressed_so_we_can_add_to_landscape = true
+	
+	var consider_stop_sliding = false
+	if Input.is_action_just_released("left") and dslide.x == -1: 
+		dslide.x = 0
+		consider_stop_sliding = true
+	if Input.is_action_just_released("right") and dslide.x == 1:
+		dslide.x = 0
+		consider_stop_sliding = true
+	if Input.is_action_just_released("down") and dslide.y == 1: 
+		dslide.y = 0
+		consider_stop_sliding = true
+	if consider_stop_sliding and dslide == Vector2.ZERO: 
+		stop_sliding()
 	
 	if Input.is_action_just_pressed("rotate"):
 		var rotation_succeeded = attempt_rotation()
 		if rotation_succeeded: performed_an_action = true
 			
 	if attempt_a_move:
-		input_timer.start()
 		if verify_proposed_coordinates(proposed_x, proposed_y, current_piece.current_positions()):
 			set_piece_position(proposed_x, proposed_y)
 			performed_an_action = true
